de94eeb7a1d2c96fff1ac962ebf57abacb270d84
Igor Cruz <42593480+igorcruzf@users.noreply.github.com>
2018-09-28 13:44:30 -0300

    Update conversão.py

--- conversão.py
+++ conversão.py
@@ -1,28 +1,28 @@
-x = int(input()) #número na base 10
-y = int(input()) #qualquer base abaixo de 10
-z = []
+base10 = int(input()) #número na base 10
+newbase = int(input()) #qualquer base até 16
+lista = []
 resp = ''
 
-while x//y != 0 or x%y != 0:
-    z.append(x%y)
-    x = x//y
+while base10//newbase != 0 or base10%newbase != 0:
+    lista.append(base10%newbase)
+    base10 = base10//newbase
 
-z.reverse() #comando para inverter a lista e ter a verdadeira resposta na conversão
+lista.reverse() #comando para inverter a lista e ter a verdadeira resposta na conversão
 
-for i in range(len(z)):
-    if z[i] > 9: #para casos até hexadecimal
-        if z[i] == 10:
-            z[i] = 'A'
-        elif z[i] == 11:
-            z[i] = 'B'
-        elif z[i] == 12:
-            z[i] = 'C'
-        elif z[i] == 13:
-            z[i] == 'D'
-        elif z[i] == 14:
-            z[i] == 'E'
-        elif z[i] == 15:
-            z[i] == 'F'
-    resp += str(z[i]) #transformando lista para str
+for i in range(len(lista)):
+    if lista[i] > 9: #para casos até hexadecimal
+        if lista[i] == 10:
+            lista[i] = 'A'
+        elif lista[i] == 11:
+            lista[i] = 'B'
+        elif lista[i] == 12:
+            lista[i] = 'C'
+        elif lista[i] == 13:
+            lista[i] == 'D'
+        elif lista[i] == 14:
+            lista[i] == 'E'
+        elif lista[i] == 15:
+            lista[i] == 'F'
+    resp += str(lista[i]) #transformando lista para str
 
 print(resp)
